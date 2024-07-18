6306562111b156be4bfb36c969a75e4e31c6d2fa
Corey Knoettgen <cknoettg@yahoo.com>
2024-07-18 18:57:17 -0400

    add REST API GET and POST logic

--- rest_app.py
+++ rest_app.py
@@ -1,5 +1,18 @@
-# POST logic
+import web_app.py
 
+# POST logic
+@app.route('/data/<user_id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def user(user_id):
+    if request.method == 'GET':
+        return {'user_id': user_id}
+
+    elif request.method == 'POST':
+        # getting the json data payload from request
+        request_data = request.json
+        # treating request_data as a dictionary to get a specific value from key
+        user_name = request_data.get('user_name')
+        return {'user id': user_id , 'user name': user_name}
+  # todo elif for put and delete
 # GET logic
 
 # PUT logic
