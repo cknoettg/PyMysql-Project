a7112d4a7d3032476fc376cb5ee90e00f7c74ad2
Corey Knoettgen <cknoettg@yahoo.com>
2024-07-20 20:02:53 -0400

    fixed POST method

--- rest_app.py
+++ rest_app.py
@@ -2,25 +2,29 @@ import web_app
 import db_connector
 from flask import request
 
-# POST logic
+# Create route for each REST API method
 @app.route('/users/<user_id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def user(user_name):
+    # GET logic
     if request.method == 'GET':
         try:
             return {"status": "ok", "user_name": user_name}, 200
         except Exception as e:
             return 500
 
-
+    # POST logic
     elif request.method == 'POST':
-        # getting the json data payload from request
-        request_data = request.json
-        # treating request_data as a dictionary to get a specific value from key
-        user_name = request_data.get('user_name')
-        return {'user id': user_id , 'user name': user_name}
-  # todo elif for put and delete
-# GET logic
+        try:
+            # getting the json data payload from request
+            request_data = request.json
+            # treating request_data as a dictionary to get a specific value from key
+            user_name = request_data.get("user_name")
+            return {"status": "ok", "user_added": user_name}, 200
+        except Exception as e:
+            return {"status": "error", "reason": "id already exists"}, 500
 
+  # todo elif for put and delete
+    
 # PUT logic
 
 # DELETE logic
