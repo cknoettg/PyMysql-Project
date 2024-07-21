cb7bf2848edbc1c369855bdb04f04fdc88f92014
Corey Knoettgen <cknoettg@yahoo.com>
2024-07-20 20:12:01 -0400

    Added PUT logic for a single case - not generic yet

--- rest_app.py
+++ rest_app.py
@@ -23,8 +23,18 @@ def user(user_name):
         except Exception as e:
             return {"status": "error", "reason": "id already exists"}, 500
 
-  # todo elif for put and delete
-    
-# PUT logic
+    # PUT logic
+    elif request.method == 'PUT':
+        try:
+            user_id = 1
+            new_user_name = "George"
+
+            update_query = f"UPDATE users SET user_name = %s WHERE = %s"
+
+            cursor = db_connector.conn.cursor()
+            cursor.execute(update_query, (new_user_name, user_id))
+            return {"status": "ok", "user_updated": user_name}, 200
+        except Exception as e:
+            return {"status": "ok", "reason": "no such id"}, 500
 
 # DELETE logic
