5cd5c081d8844129ba5485684bda9d60fae86494
cknoettg <cknoettg@yahoo.com>
2024-07-25 21:26:37 -0400

    Added GET request SQL logic to db_connector

--- db_connector.py
+++ db_connector.py
@@ -14,5 +14,17 @@ def add_user(user_id, user_name, creation_date):
 
     cursor.execute(f"INSERT into {schema_name}.users (name, id, date) VALUES ('{user_name}', {user_id}, {creation_date})")
 
+    # Close the connection
+    cursor.close()
+    conn.close()
+
+# SQL logic for GET request
+def get_user(user_id):
+    # Getting a cursor from Database
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT user_name FROM mydb.users WHERE id = {user_id}")
+
+    # Close the connection
     cursor.close()
     conn.close()
