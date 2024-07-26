d0431bab1837421ad56d6cca4e1954dd88016243
cknoettg <cknoettg@yahoo.com>
2024-07-25 22:02:50 -0400

    Added GET logic in HTML to web_app

--- web_app.py
+++ web_app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request
+from db_connector import add_user, get_user, update_user, delete_user
 
 #create Flask app
 app = Flask(__name__)
+
+@app.route('/get_user_name', methods = ['GET'])
+def get_user_name(user_id):
+        user_name = get_user(user_id)
+        return "<H1 id='user'>" + user_name + "</H1>"
+
 
 app.run(host='127.0.0.1', debug=True, port=5000)
