e7f90c2ba63d4a25bc88e6b86b254c2aedfe9721
cknoettg <cknoettg@yahoo.com>
2024-07-28 18:47:00 -0400

    Added logic to get_user_name route

--- web_app.py
+++ web_app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 @app.route('/get_user_name', methods = ['GET'])
 def get_user_name(user_id):
         user_name = get_user(user_id)
-        return "<H1 id='user'>" + user_name + "</H1>"
+        if user_name == None:
+                return "<H1 id='error'>" no such user: + user_id + "</H1>"
+        else:
+                return "<H1 id='user'>" + user_name + "</H1>"
+
 
 
 app.run(host='127.0.0.1', debug=True, port=5000)
